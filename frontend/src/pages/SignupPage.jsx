83a051a36e6cf4f3ecc036feaa0de135f911f8d1
jonghyun0429p <bwhdgus0429@naver.com>
2024-11-13 20:26:50 +0900

    [FIX] 이메일 중복 에러 핸들링

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -60,12 +60,16 @@ function SignupPage() {
         email: formData.email
       }
     }).then((res) => {
-      if(res.status == 200){
+      if(res.status == 400){
         setEmailChecked(true)
         alert(res.data.message)
-      }else{
-        alert(res.data.message)
       }
+    }).catch((error)=>{      
+      {error.response.status == 400
+        ? alert(error.response.data.message)
+        : alert('문제가 발생하였습니다. 다시 시도해주세요.')
+      }
+
     })
   }
 
